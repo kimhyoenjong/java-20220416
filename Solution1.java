351b6c6b9f1be6259bbad914bc90c87d647fe525
kimhyoenjong <101921483+kimhyoenjong@users.noreply.github.com>
2022-04-16 14:49:55 +0900

    Add files via upload

--- Solution1.java
+++ Solution1.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Solution1 {
 	public static void main(String[] args) {
@@ -18,29 +19,60 @@ public class Solution1 {
 		System.out.println(d);
 		System.out.println(s);
 		
-		int i = 200;
-		double d = 10.5;
-		String s = "반갑습니다.";
+		
+		i = 200;
+		d = 10.5;
+		s = "반갑습니다.";
 		
 		System.out.println(i);
 		System.out.println(d);
 		System.out.println(s);
+		
 		// 2번 문제
-		int a = 5;
-	    int b = 10;
+//		int a = 5;
+//	    int b = 10;
+//
+//	    System.out.println("a : " + a);
+//	    System.out.println("b : " + b);
+//	   
+//	    // 여기서 부터 
+//	    
+//	    // 여기까지 수정 가능
+//	    
+//	    System.out.println("a : " + a);
+//	    // 출력 : a : 10
+//	    System.out.println("b : " + b);
+//	    // 출력 :  b : 5
+	    
+	    
+	    // 3번 문제
+	    Random ran = new Random();
 
+	    // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
+	    // `int` => a라는 공간에는 정수만 담을 수 있다.
+	    // `;` => 문장이 끝남을 알린다.
+	    int a;
+	    a = ran.nextInt(100);
+	    int b = ran.nextInt(100);
+	    
+	    
+	    // `+` => 문장과 문자을 합친다.
 	    System.out.println("a : " + a);
 	    System.out.println("b : " + b);
-	   
-	    // 여기서 부터 
 	    
+	    // 여기서 부터 
+	    int c;
+	    c= a;
+	    a=b;
+	    b=c;
+
+
 	    // 여기까지 수정 가능
 	    
 	    System.out.println("a : " + a);
-	    // 출력 : a : 10
+	    // 출력 : a : b의 값
 	    System.out.println("b : " + b);
-	    // 출력 :  b : 5
-	    
+	    // 출력 :  b : a의 값
 	    
 	}
 }
